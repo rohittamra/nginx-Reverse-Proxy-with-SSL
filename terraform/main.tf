a3c998e1d5e6178d9e55597d986901bf09924537
rohit tamra <rohit.tamra@rise-world.com>
2025-08-28 19:55:20 +0200

    Pushing with new changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,14 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "linux-tweet-rg-${random_string.suffix.result}"
   location = var.location
 }
 
